48c48ffeb6697f80af238bb9852192b346359293
Kenneth Sim Jing Hong <kennethddj@gmail.com>
2021-12-23 22:45:37 +0800

    initialize amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "leadsmanagementsyste1397cb211397cb21": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s349b9a620": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "leadsmanagementsyste": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
