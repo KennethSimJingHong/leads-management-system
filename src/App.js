ecc9c6c9793795c24e543e7127bc74de1780ddf1
Kenneth Sim Jing Hong <kennethddj@gmail.com>
2021-12-23 21:47:18 +0800

    setup code format

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
-import React from 'react';
-import { AppBar, Button, Toolbar, Typography, Box } from '@material-ui/core';
-import { Fragment } from 'react';
+import logo from "./logo.svg";
+import React from "react";
+import { AppBar, Button, Toolbar, Typography, Box } from "@material-ui/core";
+import { Fragment } from "react";
 
 function App() {
   return (
     <Fragment>
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography>Leads Management System</Typography>
-          </Toolbar>
-        </AppBar>
-        <Toolbar />
-        <h1>Hello World</h1>
-        <Button variant="contained" color="primary">Test Button</Button>
-        <Box color="error">Test typography</Box>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography>Leads Management System</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <h1>Hello World</h1>
+      <Button variant="contained" color="primary">
+        Test Button
+      </Button>
+      <Box color="error">Test typography</Box>
     </Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'fluxible-js';
-import {ThemeProvider} from '@material-ui/core';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "fluxible-js";
+import { ThemeProvider } from "@material-ui/core";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import theme from './theme';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./theme";
 
 createStore({
   initialStore: {
-    authUser: null
+    authUser: null,
   },
   persist: {
     syncStorage: window.localStorage,
-    restore ({authUser}){
-      return {authUser};
-    }
-  }
-})
+    restore({ authUser }) {
+      return { authUser };
+    },
+  },
+});
 
 ReactDOM.render(
-  
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
